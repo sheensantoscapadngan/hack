7c6b7f00bc28daf691d63ab52834838ac1f6e733
Sheen <sheen@symph.co>
2021-05-29 10:48:05 +0800

    Configured mongoDB

---
+++ code/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const mongoURL = config.get("mongoURL");
+
+const connectToDB = async () => {
+  console.log("MONGO URL IS", mongoURL);
+  try {
+    await mongoose.connect(mongoURL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("CONNECTED TO MONGODB");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+module.exports = {
+  connectToDB,
+};
--- code/server.js
+++ code/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const { connectToDB } = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+connectToDB();
 
 app.get("/", () => console.log("TEST"));
 
