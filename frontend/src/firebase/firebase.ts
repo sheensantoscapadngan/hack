caffa40c0910b7514d37204ac59dda19fb94a23e
Sheen <sheen@symph.co>
2021-05-29 11:14:14 +0800

    Added firebase to project

---
+++ frontend/src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+try {
+    firebase.initializeApp({
+        apiKey: "AIzaSyCAM8OqsDhKOxGX-aEZ_yOFl-FPjsG108A",
+        authDomain: "hack-88778.firebaseapp.com",
+        projectId: "hack-88778",
+        storageBucket: "hack-88778.appspot.com",
+        messagingSenderId: "451843897740",
+        appId: "1:451843897740:web:d80d9d9f5ef362f75b3ea9",
+        measurementId: "G-J9ZK6YQSY2"
+    })
+} catch (error) {
+    console.error(error);
+}
+
+export default firebase;
